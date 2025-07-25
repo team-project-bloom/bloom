19b0234a82247b088b027526227f866fe014af32
Anton <StarAntonU@Gmail.com>
2025-07-26 00:14:16 +0300

    init

---
+++ src/test/java/team_project/bloom/BloomApplicationTests.java
@@ -0,0 +1,13 @@
+package team_project.bloom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
