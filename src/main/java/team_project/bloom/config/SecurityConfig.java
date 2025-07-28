d99d78c0ba3329ed744cb79f1e7665a457c3ecab
Anton <StarAntonU@Gmail.com>
2025-07-29 00:15:58 +0300

    changed SecurityFilterChain requestMatchers to All

--- src/main/java/team_project/bloom/config/SecurityConfig.java
+++ src/main/java/team_project/bloom/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -22,13 +23,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .cors(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/wines/**",
-                                "/error",
-                                "/swagger-ui/**",
-                                "/v3/api-docs/**")
+                        .requestMatchers("/**"
+                        )
                         .permitAll()
                         .anyRequest()
                         .authenticated()
