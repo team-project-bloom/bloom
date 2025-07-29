cba7c721fd70e231ddeac842b001a24b8039edd0
Anton <StarAntonU@Gmail.com>
2025-07-29 13:41:19 +0300

    added Wine entity

---
+++ src/main/java/team_project/bloom/model/Wine.java
@@ -0,0 +1,48 @@
+package team_project.bloom.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "wines")
+@SQLDelete(sql = "UPDATE wines SET is_deleted = true WHERE id=?")
+@SQLRestriction("is_delete=false")
+public class Wine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private BigDecimal price;
+    @Column(nullable = false)
+    private Integer alcohol;
+    @Column(nullable = false)
+    private Variety variety;
+    @Column(nullable = false)
+    private Integer vintage;
+    private String grape;
+    private String region;
+    private String description;
+
+    public enum Variety {
+        RED,
+        WHITE,
+        ROSE,
+        ORANGE,
+        NATURAL,
+        PET_NAT,
+        SPARKLING
+    }
+}
