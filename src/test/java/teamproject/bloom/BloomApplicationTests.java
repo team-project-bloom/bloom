d8a3c5d3fe95a4abb9047c009acbd701232b68e3
Anton <StarAntonU@Gmail.com>
2025-08-15 16:09:48 +0300

    test BloomApplicationTests

--- src/test/java/teamproject/bloom/BloomApplicationTests.java
+++ src/test/java/teamproject/bloom/BloomApplicationTests.java
@@ -1,13 +1,13 @@
 package teamproject.bloom;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BloomApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
